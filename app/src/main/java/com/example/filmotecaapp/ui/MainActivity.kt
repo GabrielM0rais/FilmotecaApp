0b13ccf4eabfe99e6407884ee6dc91713d3e22d6
Ênio Gabriel <eniogabrie.bee@gmail.com>
2023-05-01 14:49:52 -0300

    build: config dagger hilt

---
+++ app/src/main/java/com/example/filmotecaapp/ui/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.filmotecaapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.filmotecaapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
