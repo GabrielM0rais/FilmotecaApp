7b331b7220683a844ec9b683da0a7d6ef32228c2
Ênio Gabriel <eniogabrie.bee@gmail.com>
2023-05-06 18:11:02 -0300

    fix: ajuste quando retorna fecha o app

--- app/src/main/java/com/example/filmotecaapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/filmotecaapp/ui/MainActivity.kt
@@ -5,12 +5,4 @@ import com.example.filmotecaapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 0) {
-            super.onBackPressed()
-        } else {
-            finishAffinity()
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
